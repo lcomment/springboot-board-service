4afd2e6203e44562e279fc1ce1b345fdbafedde4
lcomment <kohyunsuk98@gmail.com>
2022-08-31 11:32:55 +0900

    set: 프로젝트 생성

---
+++ src/main/java/com/lcomment/book/lcommentspringbootboard/LcommentSpringbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.lcomment.book.lcommentspringbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcommentSpringbootBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcommentSpringbootBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcomment/book/lcommentspringbootboard/LcommentSpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcomment.book.lcommentspringbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcommentSpringbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
