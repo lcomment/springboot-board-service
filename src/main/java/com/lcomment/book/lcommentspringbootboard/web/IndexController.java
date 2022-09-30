6d2b97490c006c1dd584bada309c10a31e321724
Hyunseok Ko <56003992+lcomment@users.noreply.github.com>
2022-09-30 10:34:30 +0900

    PR: Security to master (#23)
    * feat: controller에 구글 로그인 기능 추가
    
    * setting: h2 설정 추가

--- src/main/java/com/lcomment/book/lcommentspringbootboard/web/IndexController.java
+++ src/main/java/com/lcomment/book/lcommentspringbootboard/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lcomment.book.lcommentspringbootboard.web;
 
+import com.lcomment.book.lcommentspringbootboard.config.auth.dto.SessionUser;
 import com.lcomment.book.lcommentspringbootboard.domain.posts.PostsRepository;
 import com.lcomment.book.lcommentspringbootboard.service.posts.PostsService;
 import com.lcomment.book.lcommentspringbootboard.web.dto.PostsResponseDto;
@@ -9,14 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
